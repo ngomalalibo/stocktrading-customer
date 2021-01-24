5e22a84313708b6f1bd6a74c183d29c03d45f7c0
Ngo <ngomalalibo@yahoo.com>
2021-01-24 23:44:35 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/kafkainit.sh
+++ travis_scripts/kafkainit.sh
@@ -18,12 +18,12 @@ else
   echo "Kafka was not Running"
 fi
 # Start daemon.
-#echo "Starting Zookeeper"
+echo "Starting Zookeeper"
 #nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
 #sleep 10
 #echo "Starting Kafka"
 #nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties 2>/dev/null
 
-nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
+nohup $DAEMON_PATH/bin/zookeeper-server-start.sh $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
 while ! nc -z localhost 2181; do sleep 0.1; done && \
-nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties
+nohup $DAEMON_PATH/bin/kafka-server-start.sh $DAEMON_PATH/config/server.properties
