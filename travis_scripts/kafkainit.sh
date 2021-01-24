efd2fde7fe40e33e4bc54fe824d8cddb51de7721
Ngo <ngomalalibo@yahoo.com>
2021-01-24 23:36:57 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/kafkainit.sh
+++ travis_scripts/kafkainit.sh
@@ -18,12 +18,12 @@ else
   echo "Kafka was not Running"
 fi
 # Start daemon.
-echo "Starting Zookeeper"
-nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
-sleep 10
-echo "Starting Kafka"
-nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties 2>/dev/null
+#echo "Starting Zookeeper"
+#nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
+#sleep 10
+#echo "Starting Kafka"
+#nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties 2>/dev/null
 
-#$DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
-#while ! nc -z localhost 2181; do sleep 0.1; done && \
-#$DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties
+nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null && \
+while ! nc -z localhost 2181; do sleep 0.1; done && \
+nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties
