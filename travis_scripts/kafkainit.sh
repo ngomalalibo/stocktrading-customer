5c3412b556b4e06ebc74629df4460d4960c293c9
Ngo <ngomalalibo@yahoo.com>
2021-01-25 12:06:02 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/kafkainit.sh
+++ travis_scripts/kafkainit.sh
@@ -1,4 +1,5 @@
 DAEMON_PATH=/home/ubuntu/kafka_2.12-2.5.0
+PATH=$PATH:$DAEMON_PATH/bin
 
 # See how we were called.
 # Stop daemons.
@@ -19,8 +20,8 @@ fi
 # Start daemon.
 echo "Starting Zookeeper"
 nohup $DAEMON_PATH/bin/zookeeper-server-start.sh -daemon $DAEMON_PATH/config/zookeeper.properties 2>/dev/null &
-echo "Sleeping for 10 seconds"
-sleep 10
+echo "Waiting for zookeeper to start"
+while ! nc -z localhost 2181; do sleep 0.1; done && \
 echo "Starting Kafka"
 nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon /$DAEMON_PATH/config/server.properties 2>/dev/null &
 exit 0
