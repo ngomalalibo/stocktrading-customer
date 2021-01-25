8879a08d308deaa1902781d2bdd05660cf1f5794
Ngo <ngomalalibo@yahoo.com>
2021-01-25 01:11:32 +0100

    Deployment to AWS EC2 with Travis CI

--- travis_scripts/start_kafka.sh
+++ travis_scripts/start_kafka.sh
@@ -2,5 +2,5 @@ DAEMON_PATH=/home/ubuntu/kafka_2.12-2.5.0
 PATH=$PATH:$DAEMON_PATH/bin
 
 echo "Starting Kafka second time"
-nohup $DAEMON_PATH/bin/kafka-server-start.sh -daemon $DAEMON_PATH/config/server.properties 2>/dev/null &
+nohup $DAEMON_PATH/bin/kafka-server-start.sh $DAEMON_PATH/config/server.properties 2>/dev/null &
 echo "Started Kafka second time"
